c81c5185860f5752dcc34785afd87d6db778d01f
ibasan <ibarakihirosi@gmail.com>
2017-07-23 01:17:04 +0900

    Update index.js

--- stamp/src/index.js
+++ stamp/src/index.js
@@ -15,12 +15,70 @@ var _ua = (function(u){
       || u.indexOf("blackberry") != -1
   }
 })(window.navigator.userAgent.toLowerCase());
+var context,context2;
+var w=window.outerWidth;
+var h=window.outerHeight;
+var i,i2;
+
+var gearR=200;
 
 window.addEventListener("load",load);
+window.addEventListener("resize",changeSize);
 
 function load(){
 	if(_ua.Mobile||_ua.Tablet){
 		var header=document.getElementById("header");
+		header.style.top="-100px";
 		//todo:insert menu button
 	}
+	init();
+}
+function init(){
+	context=document.getElementById('con1');//context
+	context2=document.getElementById('con2');//layout
+	
+	text=document.getElementById("text");
+
+	i=document.getElementById("pic");
+	i2=document.getElementById("pic2");
+
+	changeSize();
+}
+function changeSize(){
+	w=document.body.clientWidth;
+	h=Math.min(document.documentElement.scrollHeight||document.body.scrollHeight,window.innerHeight)-17;
+
+	var con=context.style;
+	var con2=context2.style;
+
+	document.body.style.height=h+"px";
+
+//	var l=(w>350)?50:(50-((350-w)/7));
+//	var o=(w/2)-(l*3.5);
+//	con.font="normal "+l+"px 'Times New Roman'";
+
+//	c.fillStyle="rgb(0,0,0)";
+//	c.fillText("スタンプラリー",o,h/2);
+//	c.font="20px 'Times New Roman'";
+//	c.fillText('~With your smartphone you can know "FukoFes2017" that you do not know~',0/*(w/2)-(10*35)*/,h/2+l);
+
+	i.width=i.height=gearR*2.2;
+	i2.width=i2.height=gearR*1.7;
+
+	var is=i.style;
+	var is2=i2.style;
+
+	is.top=(h-(i.width/2))+"px";
+	is2.top="-"+(i2.width/2)+"px";
+	is.left="-"+(i.height/2)+"px";
+	is2.left=(w-(i2.height/2))+"px";
+}
+function changeSpeed(){
+	//力尽きたので誰か書いて～
+}
+function start(){
+	
+}
+function what(){
+	
 }
