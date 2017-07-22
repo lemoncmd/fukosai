570aad5d2e349fecc48a5611deb306a227153e62
ibasan <ibarakihirosi@gmail.com>
2017-07-23 02:06:31 +0900

    Update index.js

--- stamp/src/index.js
+++ stamp/src/index.js
@@ -46,33 +46,44 @@ function init(){
 }
 function changeSize(){
 	w=document.body.clientWidth;
-	h=Math.max(document.documentElement.scrollHeight||document.body.scrollHeight,window.innerHeight)-17;
+	h=Math.min(document.documentElement.scrollHeight||document.body.scrollHeight,window.innerHeight)-17;
 
 	var con=context.style;
 	var con2=context2.style;
 
 	document.body.style.height=h+"px";
 
-	var l=(w>350)?50:(50-((350-w)/7));
-	var o=(w/2)-(l*3.5);
-	con.font="normal "+l+"px 'Times New Roman'";
+//	var l=(w>350)?50:(50-((350-w)/7));
+//	var o=(w/2)-(l*3.5);
+//	con.font="normal "+l+"px 'Times New Roman'";
 
 //	c.fillStyle="rgb(0,0,0)";
 //	c.fillText("スタンプラリー",o,h/2);
 //	c.font="20px 'Times New Roman'";
 //	c.fillText('~With your smartphone you can know "FukoFes2017" that you do not know~',0/*(w/2)-(10*35)*/,h/2+l);
 
-	i.width=i.height=gearR*1.7;
-	i2.width=i2.height=gearR*2.2
+	i.width=i.height=gearR*2.2;
+	i2.width=i2.height=gearR*1.7;
 
 	var is=i.style;
 	var is2=i2.style;
 
-	is.top="-"+(i.width/2)+"px";
+	is.top=(h-(i.width/2))+"px";
 	is2.top="-"+(i2.width/2)+"px";
 	is.left="-"+(i.height/2)+"px";
 	is2.left=(w-(i2.height/2))+"px";
 }
 function changeSpeed(){
-
+	//力尽きたので誰か書いて～
+}
+function start(){
+	if(!localStorage.getItem("fukofes2017_str")){
+		if(!confirm("はじめにルール説明をお読みください。\n（まだ読まれていない方はキャンセルボタンを押してください）\n\nゲームを開始しますか？")){
+		return;
+		}
+		//ここにゲームスタート時の描画処理
+	}
+}
+function what(){
+	localStorage.setItem("fukofes2017_str","Have a great time!")
 }
